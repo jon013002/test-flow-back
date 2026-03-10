1fe02e80ee4bd0034c61be1c3f590325061958bd
jon013002 <jonmtz3002@gmail.com>
2026-03-10 01:11:43 -0600

    ✨ Feat(Auth): Add route

---
+++ routes/subroutes/auth.routes.php
@@ -0,0 +1,12 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+
+use Illuminate\Support\Facades\Route;
+
+Route::post('sign-up', [AuthController::class, 'register']);
+Route::post('sign-in', [AuthController::class, 'login']);
+
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::post('logout', [AuthController::class, 'logout']);
+});
