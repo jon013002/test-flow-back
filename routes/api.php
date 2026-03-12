74f55758d598840e9b099065ccff98c76b9d447d
jon013002 <jonmtz3002@gmail.com>
2026-03-12 00:16:25 -0600

    ⚡️ Feat: Add project route

--- routes/api.php
+++ routes/api.php
@@ -1,3 +1,4 @@
 <?php
 
 @include('subroutes/auth.routes.php');
+@include('subroutes/testing.routes.php');
---
+++ routes/subroutes/testing.routes.php
@@ -0,0 +1,8 @@
+<?php
+
+use App\Http\Controllers\ProjectController;
+use Illuminate\Support\Facades\Route;
+
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::apiResource('projects', ProjectController::class)->except('create', 'edit');
+});
