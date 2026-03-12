53cfbb3dcf618927bf99b0a30e6a20ebd929e3d8
jon013002 <jonmtz3002@gmail.com>
2026-03-12 17:56:23 -0600

    ⚡️ Feat: Add module route

--- routes/subroutes/testing.routes.php
+++ routes/subroutes/testing.routes.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\ModuleController;
 use App\Http\Controllers\ProjectController;
+
 use Illuminate\Support\Facades\Route;
 
 Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::apiResource('projects', ProjectController::class)->except('create', 'edit');
+    Route::apiResource('modules', ModuleController::class)->except('create', 'edit');
 });
