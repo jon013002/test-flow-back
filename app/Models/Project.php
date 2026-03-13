baeb831301a7eabae0fc6fa1741db5f059e1db99
jon013002 <jonmtz3002@gmail.com>
2026-03-13 00:49:17 -0600

    ✨ Feat(Module): Add relation

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -3,13 +3,19 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Project extends BaseModel
 {
     use HasUuids;
-    
+
     protected $fillable = [
         'name',
         'description',
     ];
+
+    public function modules(): HasMany
+    {
+        return $this->hasMany(Module::class);
+    }
 }
